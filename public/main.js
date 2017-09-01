22cd805f2644d5bc30b7dad46573e0ba2a464764
Robin Thomas <>
2017-09-01 12:40:41 -0500

    Fetches and displays deals

--- public/main.js
+++ public/main.js
@@ -2,28 +2,82 @@
 
 (function(){
 
-	var Component = (function(){
-		var message;
+	var Deal = {};
+	Deal.format = function(deal){
+		var output = {};
+		for(var propertyName in deal.properties){
+			output[propertyName] = deal.properties[propertyName].value;
+		}
+		return output;
+	}
+
+	var DealsList = (function(){
+
+		var actions = {};
+		actions.loadDeals = function(){
+			models.isLoading = true;
+			m.request({
+				url: '/deals'
+			}).then(function(response){
+				if(response.success){
+					models.deals = response.results.results.map(Deal.format);
+				}else{
+					models.serverResponse = response.message;
+				}
+				models.isLoading = false;
+			});
+		}
+
+		var events = {};
+		events.loadDeals = function(event){
+			actions.loadDeals();
+		}
+
+		var models = {
+			isLoading: false,
+			deals: [],
+			serverResponse: ''
+		}
+
+		var views = {};
+		views.headerRow = function(){
+			return m('tr', [
+				m('th', ''),
+				m('th', 'Created'),
+				m('th', 'Name'),
+				m('th', 'Probability'),
+				m('th', 'Amount'),
+				m('th', 'Close date')
+			]);
+		}
+		views.bodyRow = function(deal, index){
+			return m('tr', [
+				m('th', (models.deals.length - index)),
+				m('td', deal.createdate),
+				m('td', deal.dealname),
+				m('td', deal.probability_),
+				m('td', '$'+ deal.amount),
+				m('td', deal.closedate)
+			]);
+		}
+		views.listTable = function(){
+			return m('table', [
+				views.headerRow(),
+				models.deals.map(views.bodyRow)
+			]);
+		}
 
 		return {
-			oninit: function(){
-				m.request({
-					url: '/api'
-				}).then(function(response){
-					if(response.success){
-						message = response.message;
-					}else{
-						message = 'Errare humanum est.';
-					}
-				});
-			},
 			view: function(){
-				return m('h1', 'The API says: ' + message);
+				return [
+					m('button', {onclick: events.loadDeals}, 'Load'),
+					views.listTable()
+				]
 			}
 		}
 	})();
 
 	document.addEventListener('DOMContentLoaded', function(){
-		m.mount(document.getElementById('display'), Component);
+		m.mount(document.getElementById('dealsList'), DealsList);
 	});
 })();
--- server.rb
+++ server.rb
@@ -13,3 +13,22 @@ end
 get "/api" do
 	json({success: true, message: "This is from the API"})
 end
+
+get "/deals" do
+	params[:hapikey] ||= ENV["HAPIKEY"]
+	params[:count] ||= 100
+	params[:since] = (params[:since] || 0).to_i * 1000
+	params[:offset] ||= 0
+	begin
+		response = HTTParty.get("https://api.hubapi.com/deals/v1/deal/recent/modified", {query: params})
+		return json({
+			success: true,
+			results: response.to_h
+		})
+	rescue Exception => error
+		return json({
+			success: false,
+			message: error
+		})
+	end
+end
